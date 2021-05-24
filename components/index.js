be92057b1abf0190f32de90666e987ee2ea42b3c
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2021-05-23 20:54:52 -0400

    chore: delete old files

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import './header/Header';
-import './experiences/Experiences';
-import './projects/Projects';
-import './about/About';
-import './footer/Footer';
-import './menu/Menu';
-import './shared/fa-icon';
