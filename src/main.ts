bbc7bfd3aea5a2fd75e2dee417ecdc435c83ce88
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2021-05-24 10:35:42 -0400

    feat: migrate meta + console log from v2.0

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,55 @@
 import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app') as Element,
 });
+
+if (process.env.NODE_ENV === 'production') {
+  const now: any = new Date();
+  const uptime = now - new Date('2001-10-16').getTime(),
+    years = Math.floor(uptime / 31557600000),
+    months = Math.floor(uptime / 2629800000 - years * 12),
+    days = Math.floor(uptime / 86400000 - years * 365.25 - months * 30.4167);
+
+  const loaded = `
+                      .ohhs+:\`                     home@navn.me
+                       \`/mMMMmy/.                  ---------------------------
+                          /NMMMMMd+\`               OS: Linux + Windows
+                           oMMMMMMMMy.             Host: Navinn Ravindaran
+                 \`:/osyhdmNMMMMMMMMMMN-            Kernel: ${now.getHours()}.${now.getMinutes()}
+             -ohNMMMMMMMMMMMMMMMMMMMMMN.           Uptime: ${years} years, ${months} months, ${days} days
+          -sNMMMMMmhhyyyhmMMMMMMMMMMMMMh           Packages: 2001 (dpkg)
+        :dMMMNy+.          -+dMMMMMMMMMM.          Shell: zsh 5.8
+      -dMMMd/                 -hMMMMMMMM+ +yy/     Resolution: 3840x2160, 3840x2160
+     oMMMd:                     /MMMMMMMmmMMMMm-   Editor: vscode
+    yMMMo                        +MMMMMMMMMMMMMN\`  Font: Fira Code Retina
+   yMMM/             -hds  +yo\`   NMMMMMMMMMMMMM/  CPU: AMD Ryzen 9 3900X (24) @ 3.800GHz
+  +MMM+       .+.    -mNy \`NMM:   NMMMMMMMMMMMMM+  GPU: NVIDIA GeForce RTX 3080
+ \`NMMd       +MMd           .\`    NMMMMMMMMMMMMM.  Memory: 22147MiB / 32098MiB
+ +MMM:      /MMMM:                :sdMMMMMMMMMMy   
+ dMm+       mMMMMm\`                  \`MMMMMMMMd\`   Made with ♥
+ h:         MMMMMMm.                 oMMMMMMMd\`        
+            NMMMMMMMy-             .yMMMMMMMo      
+            yMMMMMMMMMmy+-.\` \`\`-/smMMMMMMMy.      
+            .MMMMMMMMMMMMMMMMMMMMMMMMMMmo\`        
+             /MMMMMMMMMMMMMMMMMMMMMNh+.           
+              /MMMMMMMMMMMMMNdys+:\`               
+               -dMMMMMMMMMd.                      
+                 -odMMMMMMM+                      
+                    \`/sdMMMMmo.                   
+                         .:+oyyo.                 
+`;
+
+  const onLoad = () => {
+    console.log(loaded);
+  };
+
+  if (document.readyState === 'complete') {
+    onLoad();
+  } else {
+    window.addEventListener('load', onLoad);
+    document.removeEventListener('load', onLoad);
+  }
+}
 
 export default app;
